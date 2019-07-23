092cabe1782af9efb861aebac20f064ab5d0f24d
sofia <sofia.perlari@gmail.com>
2019-07-23 10:39:59 +0200

    upload e salvataggio della foto profilo

--- resources/views/admin/user/profile.blade.php
+++ resources/views/admin/user/profile.blade.php
@@ -7,9 +7,21 @@
     <div class="card" style="width: 18rem;">
       <div class="card-body">
         @if (!empty($user->userDetail))
-          <img class="card-img-top" src="{{ asset('storage/' . $user->userDetail->profile_picture) }}" alt="Immagine profilo di {{ $user->name }}">
+          <a href="#" data-toggle="modal" data-target="#uploadImageForm">
+            <img class="card-img-top" src="{{ asset('storage/' . $user->userDetail->profile_picture) }}" alt="Immagine profilo di {{ $user->name }}">
+          </a>
         @else
           <p>Imposta la tua foto profilo</p>
+          <form action="{{ route('admin.profile_picture')}}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+              <label for="profile_img">Immagine profile:</label>
+              <input class="form-control-file" type="file" name="profile_img">
+            </div>
+            <div class="form-group">
+              <input class="form-control" type="submit" value="Salva">
+            </div>
+          </form>
         @endif
         <p class="card-text">
           <ul>
@@ -17,6 +29,31 @@
             <li>Email: {{ $user->email }}</li>
           </ul>
         </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="uploadImageForm" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Seleziona la tua foto profilo</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form action="{{ route('admin.profile_picture')}}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+              <label for="profile_img">Immagine profile:</label>
+              <input class="form-control-file" type="file" name="profile_img">
+            </div>
+            <div class="form-group">
+              <input class="form-control" type="submit" value="Salva">
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin/profile', 'Admin\UserController@viewProfile')->middleware('auth')->name('admin.profile');
+Route::post('/admin/saveProfilePicture', 'Admin\UserController@saveProfilePicture')->middleware('auth')->name('admin.profile_picture');
